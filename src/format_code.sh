c59d58fc285a33b50358dfc7c9ceb91cdc8932fd
SvilexArch <garmanow.kirill@yandex.ru>
2024-03-31 18:49:42 +0300

    feat: add ci

--- src/format_code.sh
+++ src/format_code.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-find runtime -iname *.h -o -iname *.cpp | xargs clang-format -style=file -i
-find test -iname *.h -o -iname *.cpp | xargs clang-format -style=file -i
-find codegen -iname *.h -o -iname *.cpp | xargs clang-format -style=file -i
+find runtime -iname *.h -o -iname *.cpp | xargs clang-format -style=Google -i
+find test -iname *.h -o -iname *.cpp | xargs clang-format -style=Google -i
+find codegen -iname *.h -o -iname *.cpp | xargs clang-format -style=Google -i
